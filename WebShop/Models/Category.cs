295bccdc46afe5480280130887c7c66d3b6abff3
mzaizai80 <89834896+mzaizai80@users.noreply.github.com>
2023-12-11 12:06:01 +0100

    Category_and_its_services_test

---
+++ WebShop/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebShop.Models.Tests
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public List<Category> Subcategories { get; set; } = new List<Category>();
+    }
+}
---
+++ WebShopTests/CategoryTests.cs
@@ -0,0 +1,128 @@
+﻿namespace WebShop.Models.Tests
+{
+    [TestFixture]
+    public class CategoryTests
+    {
+        [Test]
+        public void Category_Should_Have_Default_Values()
+        {
+            // Arrange
+            Category category = new Category();
+
+            // Assert
+            Assert.That(category.Id, Is.EqualTo(0));
+            Assert.That(category.Name, Is.EqualTo(""));
+            Assert.IsNotNull(category.Subcategories);
+            Assert.IsEmpty(category.Subcategories);
+        }
+
+        [Test]
+        public void Category_Should_Allow_Setting_Properties()
+        {
+            // Arrange
+            Category category = new Category();
+
+            // Act
+            category.Id = 1;
+            category.Name = "Test Category";
+
+            // Assert
+            Assert.That(category.Id, Is.EqualTo(1));
+            Assert.That(category.Name, Is.EqualTo("Test Category"));
+        }
+
+        [Test]
+        public void Category_Should_Allow_Adding_Subcategories()
+        {
+            // Arrange
+            Category parentCategory = new Category();
+            Category subcategory = new Category();
+
+            // Act
+            parentCategory.Subcategories.Add(subcategory);
+
+            // Assert
+            Assert.That(parentCategory.Subcategories.Count, Is.EqualTo(1));
+            Assert.That(parentCategory.Subcategories[0], Is.SameAs(subcategory));
+
+        }
+
+        [Test]
+        public void Category_Subcategories_Should_Be_Initialized()
+        {
+            // Arrange
+            Category category = new Category();
+
+            // Assert
+            Assert.IsNotNull(category.Subcategories);
+            Assert.IsEmpty(category.Subcategories);
+        }
+        
+        [Test]
+        public void Category_Subcategories_Should_Allow_Setting_Properties()
+        {
+            // Arrange
+            Category category = new Category();
+            Category subcategory = new Category();
+
+            // Act
+            subcategory.Id = 2;
+            subcategory.Name = "Subcategory";
+
+            category.Subcategories.Add(subcategory);
+
+            // Assert
+            Assert.That(category.Subcategories.Count, Is.EqualTo(1));
+            Assert.That(category.Subcategories[0].Id, Is.EqualTo(2));
+            Assert.That(category.Subcategories[0].Name, Is.EqualTo("Subcategory"));
+        }
+
+        [Test]
+        public void Category_Subcategories_Should_Allow_Adding_Subsubcategories()
+        {
+            // Arrange
+            Category category = new Category();
+            Category subcategory = new Category();
+            Category subsubcategory = new Category();
+
+            // Act
+            subcategory.Subcategories.Add(subsubcategory);
+            category.Subcategories.Add(subcategory);
+
+            // Assert
+            Assert.That(category.Subcategories.Count, Is.EqualTo(1));
+            Assert.That(category.Subcategories[0].Subcategories.Count, Is.EqualTo(1));
+            Assert.That(category.Subcategories[0].Subcategories[0], Is.SameAs(subsubcategory));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace WebShopTests
+//{
+//    internal class CategoryTests
+//    {
+//    }
+//}
