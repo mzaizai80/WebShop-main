46ddf2183d3b3dc50d3582b1908d4e6d18496f27
mzaizai80 <89834896+mzaizai80@users.noreply.github.com>
2023-12-11 16:13:48 +0100

    managing_Json_data_Relationship

---
+++ WebShop/Models/ProductCatagoryRelation.cs
@@ -0,0 +1,12 @@
+﻿namespace WebShop.Models
+{
+    public class ProductCatagoryRelation
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
