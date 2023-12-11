c5abc11223ff9fae867f7e8e00b66735eea6c2be
mzaizai80 <89834896+mzaizai80@users.noreply.github.com>
2023-12-11 11:00:31 +0100

    FileReader_adn_test

---
+++ WebShop/Services/FileReader.cs
@@ -0,0 +1,19 @@
+﻿namespace WebShop.Services
+{
+    public class FileReader 
+    {
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public string ReadAllText(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+            return ""; // Return an empty string if the file does not exist
+        }
+    }
+}
---
+++ WebShopTests/FileReaderTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using WebShop.Services;
+using System.IO;
+
+namespace WebShop.Tests
+{
+    [TestFixture]
+    public class FileReaderTests
+    {
+        private FileReader _fileReader;
+        private const string ExistingFilePath = "existing_file.txt";
+        private const string NonExistingFilePath = "non_existing_file.txt";
+        private const string FileContent = "This is a test file.";
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new FileReader();
+            // Create a test file for existing file tests
+            File.WriteAllText(ExistingFilePath, FileContent);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up test files
+            if (File.Exists(ExistingFilePath))
+            {
+                File.Delete(ExistingFilePath);
+            }
+        }
+
+        [Test]
+        public void Exists_ExistingFile_ReturnsTrue()
+        {
+            // Arrange
+            // Act
+            bool fileExists = _fileReader.Exists(ExistingFilePath);
+
+            // Assert
+            Assert.IsTrue(fileExists);
+        }
+
+        [Test]
+        public void Exists_NonExistingFile_ReturnsFalse()
+        {
+            // Arrange
+            // Act
+            bool fileExists = _fileReader.Exists(NonExistingFilePath);
+
+            // Assert
+            Assert.IsFalse(fileExists);
+        }
+
+        [Test]
+        public void ReadAllText_ExistingFile_ReturnsFileContent()
+        {
+            // Arrange
+            // Act
+            string content = _fileReader.ReadAllText(ExistingFilePath);
+
+            // Assert
+            Assert.That(content, Is.EqualTo(FileContent));
+        }
+
+        [Test]
+        public void ReadAllText_NonExistingFile_ReturnsEmptyString()
+        {
+            // Arrange
+            // Act
+            string content = _fileReader.ReadAllText(NonExistingFilePath);
+
+            // Assert
+            Assert.That(content, Is.EqualTo(string.Empty));
+        }
+    }
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace WebShopTests
+//{
+//    internal class FileReaderTests
+//    {
+//    }
+//}
